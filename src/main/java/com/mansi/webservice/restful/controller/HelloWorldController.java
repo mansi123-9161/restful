ed8171b5ff7d0135746b8ac7b91b1ea6b458d387
mansi123-9161 <mansichourasia100@gmail.com>
2021-10-13 16:21:08 +0530

    adding dependencies in pom for actuator and swagger

--- src/main/java/com/mansi/webservice/restful/controller/HelloWorldController.java
+++ src/main/java/com/mansi/webservice/restful/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -40,7 +41,7 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path ="/helloWorldInternationalization")
-	public String helloWorldInternationalization(@RequestHeader(name = "Accept-Language") Locale locale) {
-		return messageSource.getMessage("good.morning.message", null,"dell", locale);
+	public String helloWorldInternationalization(/*@RequestHeader(name = "Accept-Language") Locale locale*/) {
+		return messageSource.getMessage("good.morning.message", null,"dell", LocaleContextHolder.getLocale());
 	}
 }
