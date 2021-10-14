0dcf72653eba42d42774a3e5a7ade4899c893fa6
mansi123-9161 <mansichourasia100@gmail.com>
2021-10-14 12:00:19 +0530

    3rd way of versioning: adding in headers

--- src/main/java/com/mansi/webservice/restful/versioning/PersonController.java
+++ src/main/java/com/mansi/webservice/restful/versioning/PersonController.java
@@ -6,12 +6,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
-	@GetMapping(value = "person/param",params = "v1")
+	@GetMapping("v1/person")
 	public PersonV1 personv1() {
 		return new PersonV1("hello charlie");
 	}
-	@GetMapping(value = "person/param",params = "v2")
+	@GetMapping("v2/person")
 	public PersonV2 personv2() {
+		return new PersonV2(new Name("hello", "martie"));
+	}
+	
+	@GetMapping(value = "person/param",params = "v1")
+	public PersonV1 paramv1() {
+		return new PersonV1("hello charlie");
+	}
+	@GetMapping(value = "person/param",params = "v2")
+	public PersonV2 paramv2() {
+		/* localhost:8080/person/param?v2 */
+		return new PersonV2(new Name("hello", "martie"));
+	}
+	
+	@GetMapping(value = "person/header",headers = "v1=1")
+	public PersonV1 headerv1() {
+		return new PersonV1("hello charlie");
+	}
+	@GetMapping(value = "person/header",headers = "v2=2")
+	public PersonV2 headerv2() {
 		/* localhost:8080/person/param?v2 */
 		return new PersonV2(new Name("hello", "martie"));
 	}
