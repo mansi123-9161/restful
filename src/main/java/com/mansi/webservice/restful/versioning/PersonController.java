2a79bf587c5f08b2888cf5927a0e02dbccc53860
mansi123-9161 <mansichourasia100@gmail.com>
2021-10-14 11:35:31 +0530

    2nd way of versioning: adding in params

--- src/main/java/com/mansi/webservice/restful/versioning/PersonController.java
+++ src/main/java/com/mansi/webservice/restful/versioning/PersonController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
-	@GetMapping("v1/person")
+	@GetMapping(value = "person/param",params = "v1")
 	public PersonV1 personv1() {
 		return new PersonV1("hello charlie");
 	}
-	@GetMapping("v2/person")
+	@GetMapping(value = "person/param",params = "v2")
 	public PersonV2 personv2() {
+		/* localhost:8080/person/param?v2 */
 		return new PersonV2(new Name("hello", "martie"));
 	}
 }
